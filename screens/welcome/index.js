8145c8f6d7ce0f73f24f71cf026623a566a9fbd0
Wouter Swart <wouf.swart@gmail.com>
2023-08-13 08:42:07 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/welcome/index.js
+++ screens/welcome/index.js
@@ -1,29 +1,25 @@
-import React from "react"
-import {
-  View,
-  Image,
-  Text,
-  ScrollView,
-  SafeAreaView,
-  StyleSheet
-} from "react-native"
+import { useNavigation } from "@react-navigation/native";
+import { Pressable } from "react-native";
+import React from "react";
+import { View, Image, Text, ScrollView, SafeAreaView, StyleSheet } from "react-native";
 
 const WelcomeScreen = () => {
-  return (
-    <SafeAreaView style={styles.container}>
+  const navigation = useNavigation();
+  return <SafeAreaView style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollView}>
         <View style={styles.group} />
         <View style={styles.group}>
-          <Image style={styles.logo} source={require("./logo.png")} />
+          <Pressable onPress={() => {
+          navigation.navigate("aboutTheApp");
+        }}><Image style={styles.logo} source={require("./logo.png")} /></Pressable>
           <Text style={styles.text}>
             Let's build something amazing together!
           </Text>
         </View>
         <Text style={styles.footer}>Made with ❤️ by Crowdbotics</Text>
       </ScrollView>
-    </SafeAreaView>
-  )
-}
+    </SafeAreaView>;
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -59,6 +55,5 @@ const styles = StyleSheet.create({
     fontWeight: 700,
     marginBottom: 20
   }
-})
-
-export default WelcomeScreen
+});
+export default WelcomeScreen;
--- store/currentWeather/api.js
+++ store/currentWeather/api.js
@@ -1,9 +1,16 @@
-import axios from "axios"
+import axios from "axios";
 const currentWeather = axios.create({
   baseURL: "http://api.weatherapi.com/v1",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function currentweather_get_currentjson_read(payload) {
-  return currentWeather.get(`/current.json`)
+  return currentWeather.get(`/current.json`);
 }
-export const apiService = { currentweather_get_currentjson_read }
+
+export const apiService = {
+  currentweather_get_currentjson_read
+};
