b42568a86114738c36cb7896aa20832628099644
Crowdbotics <engineering@crowdbotics.com>
2023-08-13 06:43:31 +0000

    Update store.

--- store/wstestAPI/api.js
+++ store/wstestAPI/api.js
@@ -3,23 +3,35 @@ const wstestAPI = axios.create({
   baseURL: "https://ws-test-1-42814.botics.co",
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
+function api_docs_schema_retrieve(payload) {
+  return wstestAPI.get(`/api-docs/schema/`, { params: { lang: payload.lang } })
+}
 function api_v1_login_create(payload) {
   return wstestAPI.post(`/api/v1/login/`, payload.data)
 }
 function api_v1_signup_create(payload) {
   return wstestAPI.post(`/api/v1/signup/`, payload.data)
 }
-function rest_auth_user_retrieve(payload) {
-  return wstestAPI.get(`/rest-auth/user/`)
+function modules_articles_article_list(payload) {
+  return wstestAPI.get(`/modules/articles/article/`)
 }
-function rest_auth_user_update(payload) {
-  return wstestAPI.put(`/rest-auth/user/`, payload.data)
+function modules_articles_article_create(payload) {
+  return wstestAPI.post(`/modules/articles/article/`, payload.data)
 }
-function rest_auth_user_partial_update(payload) {
-  return wstestAPI.patch(`/rest-auth/user/`, payload.data)
+function modules_articles_article_retrieve(payload) {
+  return wstestAPI.get(`/modules/articles/article/${payload.id}/`)
 }
-function api_docs_schema_retrieve(payload) {
-  return wstestAPI.get(`/api-docs/schema/`, { params: { lang: payload.lang } })
+function modules_articles_article_update(payload) {
+  return wstestAPI.put(`/modules/articles/article/${payload.id}/`, payload.data)
+}
+function modules_articles_article_partial_update(payload) {
+  return wstestAPI.patch(
+    `/modules/articles/article/${payload.id}/`,
+    payload.data
+  )
+}
+function modules_articles_article_destroy(payload) {
+  return wstestAPI.delete(`/modules/articles/article/${payload.id}/`)
 }
 function rest_auth_login_create(payload) {
   return wstestAPI.post(`/rest-auth/login/`, payload.data)
@@ -30,34 +42,49 @@ function rest_auth_logout_retrieve(payload) {
 function rest_auth_logout_create(payload) {
   return wstestAPI.post(`/rest-auth/logout/`)
 }
-function rest_auth_registration_create(payload) {
-  return wstestAPI.post(`/rest-auth/registration/`, payload.data)
+function rest_auth_password_change_create(payload) {
+  return wstestAPI.post(`/rest-auth/password/change/`, payload.data)
 }
 function rest_auth_password_reset_create(payload) {
   return wstestAPI.post(`/rest-auth/password/reset/`, payload.data)
 }
-function rest_auth_password_change_create(payload) {
-  return wstestAPI.post(`/rest-auth/password/change/`, payload.data)
-}
 function rest_auth_password_reset_confirm_create(payload) {
   return wstestAPI.post(`/rest-auth/password/reset/confirm/`, payload.data)
+}
+function rest_auth_registration_create(payload) {
+  return wstestAPI.post(`/rest-auth/registration/`, payload.data)
 }
 function rest_auth_registration_verify_email_create(payload) {
   return wstestAPI.post(`/rest-auth/registration/verify-email/`, payload.data)
 }
+function rest_auth_user_retrieve(payload) {
+  return wstestAPI.get(`/rest-auth/user/`)
+}
+function rest_auth_user_update(payload) {
+  return wstestAPI.put(`/rest-auth/user/`, payload.data)
+}
+function rest_auth_user_partial_update(payload) {
+  return wstestAPI.patch(`/rest-auth/user/`, payload.data)
+}
 export const apiService = {
+  api_docs_schema_retrieve,
   api_v1_login_create,
   api_v1_signup_create,
-  rest_auth_user_retrieve,
-  rest_auth_user_update,
-  rest_auth_user_partial_update,
-  api_docs_schema_retrieve,
+  modules_articles_article_list,
+  modules_articles_article_create,
+  modules_articles_article_retrieve,
+  modules_articles_article_update,
+  modules_articles_article_partial_update,
+  modules_articles_article_destroy,
   rest_auth_login_create,
   rest_auth_logout_retrieve,
   rest_auth_logout_create,
-  rest_auth_registration_create,
-  rest_auth_password_reset_create,
   rest_auth_password_change_create,
+  rest_auth_password_reset_create,
   rest_auth_password_reset_confirm_create,
-  rest_auth_registration_verify_email_create
+  rest_auth_registration_create,
+  rest_auth_registration_verify_email_create,
+  rest_auth_user_retrieve,
+  rest_auth_user_update,
+  rest_auth_user_partial_update
 }
